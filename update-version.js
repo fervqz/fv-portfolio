76f3b0d7ccd11139c743b57b1e12559b8f7339ba
Fernando Vásquez <f.vasquez@softec.cloud>
2023-07-21 19:10:48 +0200

    Adding husky and asking or app version when committing

---
+++ update-version.js
@@ -0,0 +1,20 @@
+const readline = require('readline');
+const semver = require('semver');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Ingrese la nueva versión (por ejemplo, 1.2.3): ', (answer) => {
+  if (semver.valid(answer)) {
+    const newVersion = semver.clean(answer);
+    process.env.npm_package_version = newVersion;
+    console.log(`La versión se ha actualizado a ${newVersion}`);
+    rl.close();
+  } else {
+    console.log('Versión inválida. Por favor, ingrese una versión válida (por ejemplo, 1.2.3)');
+    rl.close();
+    process.exit(1);
+  }
+});
