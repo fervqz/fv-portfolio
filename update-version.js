090271290cc1abca22026fcd496a5518c6a2f515
Fernando Vásquez <f.vasquez@softec.cloud>
2023-07-21 19:20:00 +0200

    Testing pre-commit script

--- update-version.js
+++ update-version.js
@@ -1,16 +1,20 @@
 import readline from 'readline';
+import { readFileSync, writeFileSync } from 'fs';
 import semver from 'semver';
-import * as packageJson from './package.json' assert { type: "json" };
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
+// Leer el contenido del package.json
+const packageJson = JSON.parse(readFileSync('package.json', 'utf-8'));
+
 rl.question(`Ingrese la nueva versión (actual ${packageJson.version}): `, (answer) => {
   if (semver.valid(answer)) {
     const newVersion = semver.clean(answer);
-    process.env.npm_package_version = newVersion;
+    packageJson.version = newVersion; // Actualizar la versión en el objeto packageJson
+    writeFileSync('package.json', JSON.stringify(packageJson, null, 2), 'utf-8'); // Escribir el archivo actualizado
     console.log(`La versión se ha actualizado a ${newVersion}`);
     rl.close();
   } else {
